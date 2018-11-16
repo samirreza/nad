83e33f6ed9896f4a1b82b96560d31833de82d45f
Amirreza <amirreza.developer@gmail.com>
2018-11-16 15:40:52 +0330

    Check for existence of model and moduleId in CommentList widget

--- extensions/comment/widgets/commentList/CommentList.php
+++ extensions/comment/widgets/commentList/CommentList.php
@@ -2,13 +2,26 @@
 
 namespace nad\extensions\comment\widgets\commentList;
 
+use yii\base\InvalidConfigException;
 
 class CommentList extends \yii\base\Widget
 {
     public $model;
+    public $moduleId;
     public $showEditDeleteButton = true;
     public $showCreateButton = true;
     public $sort = SORT_DESC;
+
+    public function init()
+    {
+        if (empty($this->model)) {
+            throw new InvalidConfigException('model property must be set.');
+        }
+        if (empty($this->moduleId)) {
+            throw new InvalidConfigException('moduleId property must be set.');
+        }
+        parent::init();
+    }
 
     public function run()
     {
