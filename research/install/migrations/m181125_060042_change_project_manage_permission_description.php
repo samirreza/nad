5844caad477a670b5bb7b412b7ac3d546d4d40db
Amirreza Yeganegi <amirreza.developer@gmail.com>
2018-11-25 09:34:05 +0330

    write migration to change manageProject permission description

---
+++ research/install/migrations/m181125_060042_change_project_manage_permission_description.php
@@ -0,0 +1,21 @@
+<?php
+
+use yii\db\Migration;
+
+class m181125_060042_change_project_manage_permission_description extends Migration
+{
+    public function safeUp()
+    {
+        Yii::$app->db->createCommand()->update(
+            'auth_item',
+            ['description' => 'مدیریت گزارش های'],
+            ['name' => 'research.manageProject']
+        )->execute();
+    }
+
+    public function safeDown()
+    {
+        echo "m181125_060042_change_project_manage_permission_description cannot be reverted.\n";
+        return false;
+    }
+}
