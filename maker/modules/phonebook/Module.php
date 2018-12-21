c9e414c7a9876de06f6125bd3b3de868776e9eee
Hesam Khaki <h.n.khaki@gmail.com>
2018-12-22 00:17:20 +0330

    removes maker and repairer modules

--- maker/modules/phonebook/Module.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace modules\nad\maker\modules\phonebook;
-
-class Module extends \yii\base\Module
-{
-
-}
--- repairer/modules/phonebook/Module.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace modules\nad\repairer\modules\phonebook;
-
-class Module extends \yii\base\Module
-{
-
-}
