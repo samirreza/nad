47388a0a821c67dc3ae7d7cca377449f6103b4a5
Amirreza Yeganegi <amirreza.developer@gmail.com>
2019-01-18 20:01:32 +0330

    Use module and parent module in HorizontalMenuContainer

--- research/investigation/project/Module.php
+++ research/investigation/project/Module.php
@@ -5,28 +5,4 @@ namespace nad\research\investigation\project;
 class Module extends \yii\base\Module
 {
     public $defaultRoute = 'manage/index';
-    public $horizontalButtons;
-
-    public function init()
-    {
-        $this->horizontalButtons = [
-            'source' => [
-                'label' => 'منشا‌ها',
-                'url' => ['/research/investigation/source/manage/index']
-            ],
-            'proposal' => [
-                'label' => 'پروپوزال‌ها',
-                'url' => ['/research/investigation/proposal/manage/index']
-            ],
-            'project' => [
-                'label' => 'گزارش‌ها',
-                'url' => ['/research/investigation/project/manage/index']
-            ],
-            'resource' => [
-                'label' => 'منابع',
-                'url' => ['/research/investigation/resource']
-            ]
-        ];
-        parent::init();
-    }
 }
