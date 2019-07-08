6f36ac1fd9f40fe9afda41be9e24bc96d9848110
faridplus <faridplusplus@yahoo.com>
2019-07-08 22:03:21 +0430

    Started adding stage submodule to engineering module, needs more work though.

---
+++ engineering/stage/Module.php
@@ -0,0 +1,9 @@
+<?php
+namespace nad\engineering\stage;
+
+class Module extends \yii\base\Module
+{
+    public $department = 'فنی';
+    public $pluralLabel = 'مراحل';
+    public $singularLabel = 'مرحله';
+}
