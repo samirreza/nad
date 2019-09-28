350b4faaa5c561fec76ec2511240adce21a6dd66
faridplus <faridplusplus@yahoo.com>
2019-09-28 22:55:13 +0330

    Added a customized horizontal menu to reference module.

--- process/ird/sedimentation/investigation/reference/Module.php
+++ process/ird/sedimentation/investigation/reference/Module.php
@@ -7,4 +7,24 @@ use nad\process\ird\sedimentation\investigation\Module as BaseModule;
 class Module extends BaseModule
 {
     public $defaultRoute = 'manage/index';
+
+    public function init()
+    {
+        parent::init();
+        $this->horizontalMenuItems = [
+            [
+                'label' => 'داده گاه منابع',
+                'items' => [
+                    [
+                        'label' => 'لیست داده گاه منابع',
+                        'url' => ['/sedimentation/investigation/reference/manage/index']
+                    ],
+                    [
+                        'label' => 'افزودن منبع',
+                        'url' => ['/sedimentation/investigation/reference/manage/index#class_ajaxcreate']
+                    ]
+                ]
+            ]
+        ];
+    }
 }
