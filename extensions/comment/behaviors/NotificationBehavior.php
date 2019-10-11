e99394d0f3168597f8f8f20c58f545ecbccc1cb8
faridplus <faridplusplus@yahoo.com>
2019-10-11 22:53:57 +0330

    Added notification to Comment when a new comment is added.

--- extensions/comment/behaviors/NotificationBehavior.php
+++ extensions/comment/behaviors/NotificationBehavior.php
@@ -2,7 +2,10 @@
 
 namespace nad\extensions\comment\behaviors;
 
-use nad\extensions\comment\controllers\CommentController;
+use Yii;
+use yii\db\ActiveRecord;
+use yii\helpers\ArrayHelper;
+use modules\user\common\models\User;
 use nad\extensions\comment\notifications\AddCommentNotification;
 
 class NotificationBehavior extends \yii\base\Behavior
@@ -10,21 +13,56 @@ class NotificationBehavior extends \yii\base\Behavior
     public function events()
     {
         return [
-            CommentController::EVENT_INSERT_COMMENT => 'notifyExpertsAndManager',
-            CommentController::EVENT_UPDATE_COMMENT => 'notifyExpertsAndManager'
+            ActiveRecord::EVENT_AFTER_INSERT => 'notifyExpertsAndManagers'
         ];
     }
 
-    public function notifyExpertsAndManager()
+    public function notifyExpertsAndManagers($event)
     {
-        $users = [];
-        foreach ($this->owner->getExpertsQuery()->all() as $expert) {
-            $users[] = $expert->user;
+        $myClass = (new \ReflectionClass($this->owner->modelClassNameFull));
+        $myInstance = $myClass->newInstanceWithoutConstructor();
+        $sourceModel = $myInstance->findOne($this->owner->modelId);
+
+        $message = '';
+        $recipientsUsers = [];
+
+        // send to the author of $sourceModel e.g. منشا
+        $recipientsUsers[] = new User(['id' => $sourceModel->createdBy]);
+
+        //send to all superusers
+        $superUsers = User::find()->Where([
+            'type' => User::TYPE_SUPERUSER,
+            'status' => User::STATUS_ACTIVE
+        ])->all();
+        $recipientsUsers = array_merge($superUsers, $recipientsUsers);
+
+        switch ($this->owner->modelClassName) {
+            case 'Source':
+                // send to all experts assigned to the $sourceModel
+                foreach ($sourceModel->getExpertsQuery()->all() as $expert) {
+                    $recipientsUsers[] = $expert->user;
+                }
+                $message = "برای منشا «{$sourceModel->title}» نظر تازه ای ثبت شده است.";
+                break;
+            case 'Proposal':
+                $message = "برای پروپوزال «{$sourceModel->title}» نظر تازه ای ثبت شده است.";
+                break;
+            default:
+                break;
         }
+
+        // don't send to the author of this comment who has initiated this request
+        foreach ($recipientsUsers as $key => $user) {
+            if($user->id == Yii::$app->user->id)
+                unset($recipientsUsers[$key]);
+        }
+
+        // now send
         AddCommentNotification::create([
-            'source' => $this->owner,
-            'recipients' => $users,
-            'baseViewRoute' => $this->owner->getBaseViewRoute()
+            'moduleId' => $this->owner->moduleId, // TODO why is this variable needed???
+            'message' => $message,
+            'source' => $sourceModel,
+            'recipients' => $recipientsUsers
         ])->send();
     }
 }
---
+++ install/migrations/m191011_162223_alter_table_comment.php
@@ -0,0 +1,45 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Class m191011_162223_alter_table_comment
+ */
+class m191011_162223_alter_table_comment extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->execute('
+        ALTER TABLE `comment`
+            ADD COLUMN `modelClassNameFull` varbinary(255) NULL AFTER `modelId`;
+        ');
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        echo "m191011_162223_alter_table_comment cannot be reverted.\n";
+
+        return false;
+    }
+
+    /*
+    // Use up()/down() to run migration code without a transaction.
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+        echo "m191011_162223_alter_table_comment cannot be reverted.\n";
+
+        return false;
+    }
+    */
+}
