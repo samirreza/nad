e27ea49ec72e058561f77ec85eacd6ff22d08d08
faridplus <faridplusplus@yahoo.com>
2019-10-13 20:29:16 +0330

    Updated action permissions.

--- process/ird/sedimentation/ManageController.php
+++ process/ird/sedimentation/ManageController.php
@@ -2,8 +2,33 @@
 
 namespace nad\process\ird\sedimentation;
 
+use yii\helpers\ArrayHelper;
+use yii\filters\AccessControl;
+
 class ManageController extends \yii\web\Controller
 {
+    public function behaviors()
+    {
+        return ArrayHelper::merge(
+            parent::behaviors(),
+            [
+                'access' => [
+                    'class' => AccessControl::class,
+                    'rules' => [
+                        [
+                            'allow' => true,
+                            'actions' => [
+                                'index',
+                                'investigation'
+                            ],
+                            'roles' => ['sedimentation.investigation']
+                        ]
+                    ]
+                ]
+            ]
+        );
+    }
+
     public function actionIndex()
     {
         return $this->render('@nad/process/ird/sedimentation/index');
