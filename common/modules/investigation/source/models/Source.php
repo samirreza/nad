a51f4f679367df3a0e708230f65a04734f3af574
faridplus <faridplusplus@yahoo.com>
2019-10-19 20:11:19 +0330

    Getting rid of unused codes.

--- common/modules/investigation/source/models/Source.php
+++ common/modules/investigation/source/models/Source.php
@@ -2,19 +2,8 @@
 
 namespace nad\common\modules\investigation\source\models;
 
-use Yii;
-use core\behaviors\PreventDeleteBehavior;
 use extensions\file\behaviors\FileBehavior;
-use nad\office\modules\expert\models\Expert;
-use extensions\tag\behaviors\TaggableBehavior;
-use extensions\i18n\validators\JalaliDateToTimestamp;
-use nad\extensions\comment\behaviors\CommentBehavior;
 use extensions\auditTrail\behaviors\AuditTrailBehavior;
-use extensions\i18n\validators\FarsiCharactersValidator;
-use nad\common\modules\investigation\proposal\models\Proposal;
-use nad\common\modules\investigation\source\behaviors\ExpertsBehavior;
-use nad\common\modules\investigation\source\behaviors\NotificationBehavior;
-use nad\common\modules\investigation\common\behaviors\CodeNumeratorBehavior;
 
 class Source extends SourceCommon
 {
--- common/modules/investigation/source/models/SourceArchived.php
+++ common/modules/investigation/source/models/SourceArchived.php
@@ -2,18 +2,6 @@
 
 namespace nad\common\modules\investigation\source\models;
 
-use Yii;
-use core\behaviors\PreventDeleteBehavior;
-use nad\office\modules\expert\models\Expert;
-use extensions\tag\behaviors\TaggableBehavior;
-use extensions\i18n\validators\JalaliDateToTimestamp;
-use nad\extensions\comment\behaviors\CommentBehavior;
-use extensions\i18n\validators\FarsiCharactersValidator;
-use nad\common\modules\investigation\proposal\models\Proposal;
-use nad\common\modules\investigation\source\behaviors\ExpertsBehavior;
-use nad\common\modules\investigation\source\behaviors\NotificationBehavior;
-use nad\common\modules\investigation\common\behaviors\CodeNumeratorBehavior;
-
 class SourceArchived extends SourceCommon
 {
     public $moduleId = 'source';
