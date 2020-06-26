05eb535037eb8f7e7641d9330d4ddce5467073d6
faridplus <faridplusplus@yahoo.com>
2020-06-26 21:35:47 +0430

    Remove set-expert permission. All experts are selectable from now on.

--- process/ird/filter/investigationDesign/proposal/views/manage/set-expert.php
+++ process/ird/filter/investigationDesign/proposal/views/manage/set-expert.php
@@ -7,5 +7,5 @@ use nad\office\modules\expert\models\Expert;
 <?= $this->render('@nad/common/modules/investigation/proposal/views/proposal/set-expert', [
     'model' => $model,
     'departmentId' => Expert::DEPARTMENT_PROCESS,
-    'permission' => 'filter.investigationDesign'
+    'permission' => 'expert'
 ]) ?>
